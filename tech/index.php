e6e92576ffd128d92c61c026197c93cf590be32c
bitsound <62582057+bitsound@users.noreply.github.com>
2020-03-24 06:08:29 +0300

    Add files via upload

---
+++ tech/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>The website is under maintenance, please check back later.</title>
+      <link rel="stylesheet" href="tech/css/style.css">
+	   <link media="screen" href="tech/demo/styles/demo.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+
+  <div id="world">
+  </div>
+  <script src='tech/three.js'></script>
+  <script  src="tech/js/index.js"></script>
+</body>
+</html>
